d4d2151f325fb8b5291e04b46e545425d0769c5f
egorpanfilo1994 <egorpanfilo1994@gmail.com>
2021-08-03 15:03:57 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let a = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let c;
+
+    if (number < 20) {
+        c = a[number];
+        return c;
+    } else if (number >= 20 && number < 100) {
+        c = b[Math.trunc(number / 10)];
+        if (number % 10 > 0) {
+            c += ' ' + toReadable(number % 10);
+        }
+    } else if (number >= 100) {
+        c = a[Math.trunc(number / 100)] + ' hundred';
+        if (number % 100 > 0) {
+            c += ' ' + toReadable(number % 100);
+        }
+    }
+
+    return c;
+
 }
